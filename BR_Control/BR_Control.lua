3ec959aaead3ef360576a4f705693f90c80f17ed
pja1982 <pja1982@gmail.com>
2014-08-30 23:51:05 +1000

    Update BR_Control.lua

--- BR_Control/BR_Control.lua
+++ BR_Control/BR_Control.lua
@@ -77,12 +77,12 @@ local function onKeyDown(opt)
     br.setActive(true)
   elseif opt == keyboard.keys.up then
     if (rodLevel > 0) then
-		  rodLevel = rodLevel - 10
+		  rodLevel = rodLevel - 1
 			br.setAllControlRodLevels(rodLevel)
 		end
   elseif opt == keyboard.keys.down then
     if (rodLevel < 100) then
-		  rodLevel = rodLevel + 10
+		  rodLevel = rodLevel + 1
 			br.setAllControlRodLevels(rodLevel)
 		end
   elseif opt == keyboard.keys.pageDown then
@@ -114,23 +114,14 @@ while running do
 	local reactorTemp = br.getCasingTemperature()
 	term.setCursor(1,1)
 	print("    Item     Name:          Level:           Temperature:          Fuel:          Max Fuel          Waste:")
-	printXY(4, 1, "    1:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(0), br.getControlRodLevel(0), br.getFuelTemperature(0), br.getFuelAmount(0), br.getFuelAmountMax(0), br.getWasteAmount(0))
-	printXY(6, 1, "    2:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(5), br.getControlRodLevel(5), br.getFuelTemperature(5), br.getFuelAmount(5), br.getFuelAmountMax(5), br.getWasteAmount(5))
-	printXY(8, 1, "    3:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(3), br.getControlRodLevel(3), br.getFuelTemperature(3), br.getFuelAmount(3), br.getFuelAmountMax(3), br.getWasteAmount(3))
-	printXY(10, 1, "    4:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(6), br.getControlRodLevel(6), br.getFuelTemperature(6), br.getFuelAmount(6), br.getFuelAmountMax(6), br.getWasteAmount(6))
-	printXY(12, 1, "    5:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(4), br.getControlRodLevel(4), br.getFuelTemperature(4), br.getFuelAmount(4), br.getFuelAmountMax(4), br.getWasteAmount(4))
-	printXY(14, 1, "    6:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(2), br.getControlRodLevel(2), br.getFuelTemperature(2), br.getFuelAmount(2), br.getFuelAmountMax(2), br.getWasteAmount(2))
-	printXY(16, 1, "    7:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(7), br.getControlRodLevel(7), br.getFuelTemperature(7), br.getFuelAmount(7), br.getFuelAmountMax(7), br.getWasteAmount(7))
-	printXY(18, 1, "    8:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(1), br.getControlRodLevel(1), br.getFuelTemperature(1), br.getFuelAmount(1), br.getFuelAmountMax(1), br.getWasteAmount(1))
-	printXY(20, 1, "    9:       %s           %03d              %04d                  %06d         %06d            %04d",br.getControlRodName(8), br.getControlRodLevel(8), br.getFuelTemperature(8), br.getFuelAmount(8), br.getFuelAmountMax(8), br.getWasteAmount(8))
-	printXY(22, 1, "   10:       Reactor        %03d              %04d                  %06d         %06d            %04d", br.getControlRodLevel(0), br.getCasingTemperature(), br.getFuelAmount(), br.getFuelAmountMax(), br.getWasteAmount())
-	printXY(25, 1, "   11:       Energy Stored           %06.2f", br.getEnergyStored())
-	printXY(26, 1, "   12:       Energy Last Tick           %06.2f", br.getEnergyProducedLastTick())
+	printXY(4, 1, "   10:       Reactor        %03d              %04d                  %06d         %06d            %04d", br.getControlRodLevel(0), br.getCasingTemperature(), br.getFuelAmount(), br.getFuelAmountMax(), br.getWasteAmount())
+	printXY(5, 1, "   11:       Energy Stored           %06.2f", br.getEnergyStored())
+	printXY(6, 1, "   12:       Energy Last Tick           %06.2f", br.getEnergyProducedLastTick())
 	
 	centerF(40, "Data updates every second     Tick Count:  %2d", tickCnt)
 	centerF(41, "Current up time:             %2d hours %2d min", hours, mins)
 	center(42, "Left - Turn Reactor Off     Right - Turn Reactor On")
-	center(43, "Up - Pull Rods 10%          Down - Lower Rods 10%  ")
+	center(43, "Up - Pull Rods 1%           Down - Lower Rods 1%  ")
 	center(44, "Page Down - Eject Waste     Q - Quit               ")
 	--center(39, "  ")
 	
